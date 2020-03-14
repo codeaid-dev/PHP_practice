37c5c485c1dd418831d2471b40807b201ba463c6
codeaid-dev <codeaid.lab@gmail.com>
2020-03-14 10:39:16 +0900

    fix kecho.php

--- kencho.php
+++ kencho.php
@@ -8,4 +8,15 @@
                     '神奈川県' => '横浜');
 
   $prefs = array('北海道', '青森県', '岩手県', '宮城県', '栃木県', '群馬県', '神奈川県');
-  
+  $ken = $prefs[rand(0, 6)];
+  print($ken . "の県庁所在地は？\n");
+  $answer = trim(fgets(STDIN));
+  foreach ($questions as $key => $value) {
+    if ($ken == $key) {
+      if ($answer == $value) {
+        print("正解！\n" . "その通りです。\n");
+      } else {
+        print("不正解・・・\n" . "{$key}の県庁所在地は{$value}です。\n");
+      }
+    }
+  }
--- selection_sort.php
+++ selection_sort.php
@@ -1,6 +1,6 @@
 <?php
   $list = array(8, 5, 3, 6, 1, 9, 2, 7, 10, 4);
-  for($i = 0; $i < count($list); $i++ ) {
+  for ($i = 0; $i < count($list); $i++ ) {
     $min = $i;
     for ($s = $i+1; $s < count($list); $s++) {
         if ($list[$s] < $list[$min]) {
