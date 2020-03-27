09f5a3f4aaea4d4bfbce7412441228f693c83b33
codeaid-dev <codeaid.lab@gmail.com>
2020-03-27 21:46:31 +0900

    fix htmlspecialchars

--- form_get.php
+++ form_get.php
@@ -1,3 +1,5 @@
 <?php
-$comment = $_GET["comment"];
+//$comment = $_GET["comment"];
+// XSS対策
+$comment = htmlspecialchars($_GET["comment"], ENT_QUOTES, "UTF-8");
 print($comment);
--- form_post.php
+++ form_post.php
@@ -1,3 +1,5 @@
 <?php
-$comment = $_POST["comment"];
+//$comment = $_POST["comment"];
+// XSS対策
+$comment = htmlspecialchars($_POST["comment"], ENT_QUOTES, "UTF-8");
 print($comment);
