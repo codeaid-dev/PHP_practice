31a7b446aa17fd1d1d7a8478ace86853f5c23893
codeaid-dev <codeaid.lab@gmail.com>
2020-09-13 19:49:16 +0900

    add

---
+++ Exercise_array/s_array14.php
@@ -0,0 +1,22 @@
+<?php
+$alp = array("A","B","C","D","E","F","G",
+            "H","I","J","K","L","M","N",
+            "O","P","Q","R","S","T","U",
+            "V","W","X","Y","Z");
+$hide = rand(0,9);
+//$hide = array_rand($alp, 1);
+$str = "";
+foreach($alp as $key => $value) {
+  if ($key != $hide) {
+    $str .= $value;
+  }
+}
+print $str."\n";
+print "抜けているのは？";
+$ans = trim(fgets(STDIN));
+if ($ans == $alp[$hide]) {
+  print "正解！\n";
+} else {
+  print "不正解\n";
+}
+
