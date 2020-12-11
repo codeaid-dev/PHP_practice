81c4cdc46dd1281b320d9aa2152ec6f6aafc21f3
codeaid-dev <codeaid.lab@gmail.com>
2020-12-11 16:39:22 +0900

    fix

--- sql_practice.sql
+++ sql_practice.sql
@@ -11,9 +11,9 @@ UPDATE 書籍 SET 書籍名='XXXXX'
 /*** 6. ***/
 UPDATE 書籍 SET 価格=3000,発売日='2020-12-01'
 /*** 7. ***/
-INSERT INTO 書籍(ISBN,書籍名,価格,ページ数,発売日) VALUES ('9784295005094','SQL入門',2800,488,'2018-11-30');
-INSERT INTO 書籍(ISBN,書籍名,価格,ページ数,発売日) VALUES ('9784873117935','初めてのPHP',3000,364,'2017-3-18');
-INSERT INTO 書籍(ISBN,書籍名,価格,ページ数,発売日) VALUES ('9784295006329','Python入門',2400,376,'2019-6-13');
+INSERT INTO 書籍(ISBN,書籍名,価格,ページ数,発売日) VALUES ('9784295005094','SQL入門',2800,488,'2018-11-30')
+INSERT INTO 書籍(ISBN,書籍名,価格,ページ数,発売日) VALUES ('9784873117935','初めてのPHP',3000,364,'2017-3-18')
+INSERT INTO 書籍(ISBN,書籍名,価格,ページ数,発売日) VALUES ('9784295006329','Python入門',2400,376,'2019-6-13')
 /*** 8. ***/
 SELECT * FROM 書籍 WHERE ISBN='9784873117935'
 /*** 9. ***/
@@ -37,9 +37,9 @@ SELECT * FROM 書籍 WHERE 発売日>='2019-04-01' AND 発売日<='2020-03-31'
 /*** 18. ***/
 SELECT * FROM 書籍 WHERE 発売日>='2020-01-01' AND 価格<=2000
 /*** 19. ***/
-SELECT * FROM 書籍 WHERE (分類='2' AND 価格<=2000) OR (分類='1' AND 価格<=1000)
+SELECT * FROM 書籍 WHERE (分類=2 AND 価格<=2000) OR (分類=1 AND 価格<=1000)
 /*** 20. ***/
-SELECT * FROM 書籍 WHERE ISBN LIKE '9784_________' OR 書籍名 LIKE 'は___　%L'
+SELECT * FROM 書籍 WHERE ISBN LIKE '9784_________' OR 書籍名 LIKE 'は___%L'
 /*** 21. ***/
 SELECT * FROM 注文 WHERE ISBN='9784295005094' OR ISBN='9784873117935'
 /*** 22. ***/
@@ -47,7 +47,7 @@ SELECT ISBN FROM 注文 WHERE 注文日>='2020-05-01' AND 注文日<'2020-06-01'
 /*** 23. ***/
 SELECT * FROM 注文 WHERE 数量>=10 OR ポイント割引料 IS NOT NULL
 /*** 24. ***/
-SELECT * FROM 注文 ポイント割引料 IS NULL
+SELECT * FROM 注文 WHERE ポイント割引料 IS NULL
 /*** 25. ***/
 SELECT 注文日,ISBN,数量 FROM 注文 WHERE 数量>20
 /*** 26. ***/
@@ -61,7 +61,7 @@ SELECT 注文日 FROM 注文 ORDER BY 注文日 DESC OFFSET 0 FETCH FIRST 10 ROW
 /*** 30. ***/
 SELECT ISBN,価格/100 AS 百円単位の価格 FROM 書籍 WHERE 価格>=5000
 /*** 31. ***/
-SELECT DISTINCT 分類 AS 分類コード, CASE 分類 WHEN '1' THEN '理工' WHEN '2' THEN 'プログラム' WHEN '3' THEN '資格' END AS '分類別' FROM 書籍
+SELECT DISTINCT 分類 AS 分類コード, CASE 分類 WHEN '1' THEN '理工' WHEN '2' THEN 'プログラム' WHEN '3' THEN '資格' END AS 分類別 FROM 書籍
 /*** 32. ***/
 SELECT ISBN,書籍名, CASE WHEN 価格<1000 THEN 'C' WHEN 価格>=1000 AND 価格<3000 THEN 'B' ELSE 'A' END AS 価格ランク FROM 書籍
 /*** 33. ***/
@@ -73,7 +73,7 @@ SELECT 書籍名,価格,価格*1.1 AS 税込価格 FROM 書籍 ORDER BY 価格 D
 /*** 36. ***/
 SELECT DISTINCT 分類, CASE 分類 WHEN '1' THEN '理工' WHEN '2' THEN 'プログラム' WHEN '3' THEN '資格' END AS 分類名 FROM 書籍
 /*** 37. ***/
-SELECT 書籍名, SUBSTRING(ISBN,1,3) || '-' || SUBSTRING(ISBN,4,1) || '-' || SUBSTRING(ISBN,5,3) || '-' || SUBSTRING(ISBN,6,5) || '-' || SUBSTRING(13,1) AS ISBN
+SELECT SUBSTRING(ISBN,1,3) || '-' || SUBSTRING(ISBN,4,1) || '-' || SUBSTRING(ISBN,5,3) || '-' || SUBSTRING(ISBN,6,5) || '-' || SUBSTRING(ISBN,13,1) AS ISBN, 書籍名 FROM 書籍
 /*** 38. ***/
 UPDATE 書籍 SET 発売日=CURRENT_DATE WHERE ISBN='9784295005094'
 /*** 39. ***/
@@ -87,7 +87,7 @@ CREATE TABLE 文房具 (
   価格 INTEGER DEFAULT 0 CHECK(価格>=0),
   分類 INTEGER DEFAULT 4 NOT NULL,
   購入日 DATE NOT NULL
-);
+)
 /*** 42. ***/
 DROP TABLE 文房具
 /*** 43. ***/
