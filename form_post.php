938af38a5aba534540e5df983be08f633d8cff59
codeaid-dev <codeaid.lab@gmail.com>
2021-01-18 21:39:38 +0900

    remove and fix

--- form_post.php
+++ form_post.php
@@ -1,5 +1,20 @@
 <?php
-//$comment = $_POST["comment"];
-// XSS対策
-$comment = htmlspecialchars($_POST["comment"], ENT_QUOTES, "UTF-8");
-print($comment);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $comment = htmlspecialchars($_POST["comment"], ENT_QUOTES, "UTF-8");
+  print($comment);
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>入力内容表示</title>
+</head>
+<body>
+  <h1>入力内容表示</h1>
+  <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
+    <input type="text" name="comment">
+    <button type="submit">送信</button>
+  </form>
+</body>
+</html>
