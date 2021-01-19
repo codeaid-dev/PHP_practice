8adc7d47808b65bbcd174144cac7fe533c498ee7
codeaid-dev <codeaid.lab@gmail.com>
2021-01-19 09:40:25 +0900

    fix

--- leap_year_check.php
+++ leap_year_check.php
@@ -10,7 +10,6 @@ function leap_year_check($year) {
   return false;
 }
 
-//$year = $_GET['year'];
 $year = filter_input(INPUT_GET, "year", FILTER_VALIDATE_INT);
 ?>
 
@@ -21,8 +20,9 @@ $year = filter_input(INPUT_GET, "year", FILTER_VALIDATE_INT);
 <?php if (empty($year)): ?>
   <p>判定する年を入力してください</p>
   <form action="leap_year_check.php" method="get">
-    <lavel>判定年：</lavel>
+    <label>判定年：
     <input name="year" type="number" value="<? echo htmlentities(date("Y"), ENT_QUOTES, "UTF-8") ?>">
+    </label>
     <button type="submit">送信</button>
   </form>
 <?php else: ?>
