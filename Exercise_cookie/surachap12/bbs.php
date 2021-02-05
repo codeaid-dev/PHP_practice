6b306581d0e8dc267b7076d6e09f158d8c6eaa51
codeaid-dev <codeaid.lab@gmail.com>
2021-02-05 23:34:19 +0900

    add fix

--- Exercise_cookie/surachap12/bbs.php
+++ Exercise_cookie/surachap12/bbs.php
@@ -34,7 +34,7 @@ try {
   <h1>掲示板</h1>
   <p><a href="index.php">トップページに戻る</a></p>
   <form action="write.php" method="post">
-    <p>名前：<input type="text" name="name"></p>
+    <p>名前：<input type="text" name="name" value="<?php echo $_COOKIE['name'] ?>"></p>
     <p>タイトル：<input type="text" name="title"></p>
     <textarea name="body"></textarea>
     <p>削除パスワード（数字4桁）：<input type="text" name="pass"></p>
---
+++ Exercise_cookie/surachap12/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="js">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>テニスサークル交流サイト</title>
+</head>
+<body>
+  <h1>テニスサークル交流サイト</h1>
+  <p><a href="bbs.php">掲示板</a></p>
+</body>
+</html>
