5827af6ab277c2695e99ab33c763b7a3cd84c943
codeaid-dev <codeaid.lab@gmail.com>
2021-03-19 17:29:44 +0900

    fix

--- Practice/hit_and_blow_ex.php
+++ Practice/hit_and_blow_ex.php
@@ -23,24 +23,25 @@ while (true) {
 
   // 入力した4桁とランダムに選んだ4桁を比較する
   // 同じ数値が存在することを考慮して$check[]配列に情報を入れる
+  // hitチェック
   for ($i = 0; $i < count($nums); $i++) {
+    if ($nums[$i] == $digits[$i]) {
+      $hit++;
+      $check[$i] = "hit";
+    }
+  }
+  // blowチェック
+  for ($i = 0; $i < count($nums); $i++) {
+    if ($check[$i] == "hit") continue;
     for ($j = 0; $j < count($nums); $j++) {
-      if ($nums[$i] == $digits[$j]) {
-        if ($i == $j) {
-          $hit++;
-          $check[$j] = "hit";
-          break;
-        } else {
-          if ($check[$j] != "blow" && $check[$j] != "hit") {
-            $blow++;
-            $check[$j] = "blow";
-            break;
-          }
+      if ($nums[$j] == $digits[$i]) {
+        if ($check[$j] != "hit" && $check[$j] != "blow") {
+          $blow++;
+          $check[$j] = "blow";
         }
       }
     }
   }
-
   if ($hit == 4) {
     print "クリア！！\n";
     break;
