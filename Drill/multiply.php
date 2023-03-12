9bb878a1735fffe7eabde39004fd64293f324008
codeaid-dev <codeaid.lab@gmail.com>
2023-03-12 17:24:30 +0900

    add

---
+++ Drill/multiply.php
@@ -0,0 +1,33 @@
+<?php
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $num = filter_input(INPUT_POST, 'num', FILTER_VALIDATE_INT,
+                        array('options' => array('min_range' => 1,'max_range' => 99)));
+    if ($num!=null && $num!=false) {
+      $result = '';
+      for ($i=1;$i<=$num;$i++) {
+        if ($i > $num/$i) break;
+        if ($num % $i == 0) $result .= $i."x".intval($num/$i)."=".$num." ";
+      }
+    } else {
+      $result = '1~99の整数を入力してください。';
+    }
+  }
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHPドリル</title>
+</head>
+<body>
+  <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
+    <label>答え：<input type="text" name="num"></label><br>
+    <br>
+    <button type="submit">表示</button>
+  </form>
+  <h2>結果表示</h2>
+  <?php $result = $result ?? ''; ?>
+  <p><?php echo htmlspecialchars($result); ?></p>
+</body>
+</html>
---
+++ Drill/palindrome.php
@@ -0,0 +1,33 @@
+<?php
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $str = $_POST['str'] ?? '';
+    $reverse = '';
+    $length = mb_strlen($str);
+    for ($i=$length-1; $i>=0; $i--) {
+      $reverse .= mb_substr($str,$i,1);
+    }
+    if ($str==$reverse) {
+      $result = "「{$str}」は回文です。";
+    } else {
+      $result = "「{$str}」は回文ではありません。";
+    }
+  }
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHPドリル</title>
+</head>
+<body>
+  <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
+    <label>入力：<input type="text" name="str"></label><br>
+    <br>
+    <button type="submit">判定</button>
+  </form>
+  <h2>結果表示</h2>
+  <?php $result = $result ?? ''; ?>
+  <p><?php echo htmlspecialchars($result); ?></p>
+</body>
+</html>
