467c511763b82dad7df4d91ed6caff897f0c3fae
codeaid-dev <codeaid.lab@gmail.com>
2024-02-09 10:00:05 +0900

    fix

--- Drill/drill15.php
+++ Drill/drill15.php
@@ -1,31 +1,31 @@
 <?php
   if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $bottom = filter_input(INPUT_POST, 'bottom', FILTER_VALIDATE_INT);
-    $height = filter_input(INPUT_POST, 'height', FILTER_VALIDATE_INT);
-    $top = filter_input(INPUT_POST, 'top', FILTER_VALIDATE_INT);
-    $radius = filter_input(INPUT_POST, 'radius', FILTER_VALIDATE_INT);
+    $bottom = filter_input(INPUT_POST, 'bottom');
+    $height = filter_input(INPUT_POST, 'height');
+    $top = filter_input(INPUT_POST, 'top');
+    $radius = filter_input(INPUT_POST, 'radius');
     $area = $_POST['area'] ?? 1;
     if ($area == 1) { // 四角形
-      if (empty($bottom) || empty($height)) {
-        $error = '整数を入力してください。';
+      if (empty($bottom) || !is_numeric($bottom) || empty($height) || !is_numeric($height)) {
+        $error = '数字を入力してください。';
       } else {
         $result = '四角形の面積は'.($bottom * $height);
       }
     } else if ($area == 2) { // 三角形
-      if (empty($bottom) || empty($height)) {
-        $error = '整数を入力してください。';
+      if (empty($bottom) || !is_numeric($bottom) || empty($height) || !is_numeric($height)) {
+        $error = '数字を入力してください。';
       } else {
         $result = '三角形の面積は'.($bottom * $height / 2);
       }
     } else if ($area == 3) { // 台形
-      if (empty($bottom) || empty($height) || empty($top)) {
-        $error = '整数を入力してください。';
+      if (empty($bottom) || !is_numeric($bottom) || empty($height) || !is_numeric($height) || empty($top) || !is_numeric($top)) {
+        $error = '数字を入力してください。';
       } else {
         $result = '台形の面積は'.(($top + $bottom) * $height / 2);
       }
     } else if ($area == 4) { // 円
-      if (empty($radius)) {
-        $error = '整数を入力してください。';
+      if (empty($radius) || !is_numeric($radius)) {
+        $error = '数字を入力してください。';
       } else {
         $result = '円の面積は'.($radius * $radius * 3.14);
       }
@@ -50,10 +50,10 @@
     <label>上底：<input type="text" name="top" value="<?= $top ?? '' ?>">cm</label><br>
     <label>半径：<input type="text" name="radius" value="<?= $radius ?? '' ?>">cm</label><br>
     <p>*円周率は3.14で計算します。</p>
-    <label>四角形<input type="radio" name="area" value="1" checked></label>
-    <label>三角形<input type="radio" name="area" value="2"></label>
-    <label>台形<input type="radio" name="area" value="3"></label>
-    <label>円<input type="radio" name="area" value="4"></label><br>
+    <label><input type="radio" name="area" value="1" checked>四角形</label>
+    <label><input type="radio" name="area" value="2">三角形</label>
+    <label><input type="radio" name="area" value="3">台形</label>
+    <label><input type="radio" name="area" value="4">円</label><br>
     <br>
     <button type="submit">計算</button>
   </form>
--- Drill/drill19.php
+++ Drill/drill19.php
@@ -1,6 +1,6 @@
 <?php
   if (!isset($_GET['page'])) {
-    header('Location: pages.php?page=1');
+    header('Location: drill19.php?page=1');
     exit;
   }
 ?>
@@ -12,13 +12,17 @@
   <title>PHPドリル</title>
 </head>
 <body>
-  <p style="font-size:40px;">ページ<?php echo $_GET['page'] ?>の画面</p>
-  <?php for ($i=1; $i<=5; $i++) { ?>
-    <?php if ($i != $_GET['page']) { ?>
-      <span style="margin:5px;"><a href="pages.php?page=<?= $i ?>"><?= $i ?></a></span>
-    <?php } else { ?>
-      <span style="margin:5px;"><?= $i ?></span>
+  <?php if ($_GET['page'] <= 5) { ?>
+    <p style="font-size:40px;">ページ<?php echo $_GET['page'] ?>の画面</p>
+    <?php for ($i=1; $i<=5; $i++) { ?>
+      <?php if ($i != $_GET['page']) { ?>
+        <span style="margin:5px;"><a href="drill19.php?page=<?= $i ?>"><?= $i ?></a></span>
+      <?php } else { ?>
+        <span style="margin:5px;"><?= $i ?></span>
+      <?php } ?>
     <?php } ?>
+  <?php } else { ?>
+    <h2>ページはありません。</h2>
   <?php } ?>
 </body>
 </html>
